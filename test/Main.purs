0fdca690656de1083e1ba961746a7b9b83b8d4e8
Alex Mouton <alex.mouton@noble.ai>
2020-08-07 22:42:48 -0700

    Remove Main

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,17 @@
-module Test.Main where
+module Main where
 
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Cypress (Cy, get, go, should, typ, runCypress, visit, first, click)
 
-main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main :: Cy -> Effect Unit
+main = runCypress $ do
+  visit "/"
+  _ <- get "a.a-carousel-goto-prevpage"
+       >>= should "have.length" 1
+       >>= first
+       >>= click
+  _ <- get "#twotabsearchtextbox" >>= typ "HEHEHE"
+  visit "/dp/B07X6C9RMF/ref=ods_gw_vicc_blinkmini_apr_anc"
+  go "back"
